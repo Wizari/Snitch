d1b7625c0316d35b58ab9c85b63b98f5f46fab96
wizari <git301398>
2020-11-04 15:57:17 +0300

    0.3.01

--- src/main/java/com/gmail/wizaripost/snitch/mail/PropertiesProviderFromMemory.java
+++ src/main/java/com/gmail/wizaripost/snitch/mail/PropertiesProviderFromMemory.java
@@ -23,11 +23,20 @@ public class PropertiesProviderFromMemory implements IPropertiesProvider {
 //        properties.put("mail.smtp.port", "587");
 //        properties.put("mail.smtp.auth", "true");
 //        properties.put("mail.smtp.starttls.enable", "true");
+//
+//        properties.put("mail.smtp.host", host);
+//        properties.put("mail.smtp.port", port);
+//        properties.put("mail.smtp.auth", auth);
+//        properties.put("mail.smtp.starttls.enable", starttlsEnable);
+
 
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", port);
         properties.put("mail.smtp.auth", auth);
         properties.put("mail.smtp.starttls.enable", starttlsEnable);
+        properties.put("mail.smtp.socketFactory.port", port);
+        properties.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.socketFactory.fallback", "false");
         return properties;
     }
 }
